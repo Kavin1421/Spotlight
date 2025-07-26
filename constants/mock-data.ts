ce498244a908ad5c052ac2fd3bd3fcf36fd76b5e
Kavinkumar <kavinkumar@Kavinkumars-Mac-mini.local>
2025-07-27 00:57:41 +0530

    CreatePost

---
+++ constants/mock-data.ts
@@ -0,0 +1,50 @@
+export const STORIES = [
+  {
+    id: "1",
+    username: "You",
+    avatar: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=400&fit=crop",
+    hasStory: false,
+  },
+  {
+    id: "2",
+    username: "Dilip.N",
+    avatar: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&h=400&fit=crop",
+    hasStory: true,
+  },
+  {
+    id: "3",
+    username: "Roshan.M",
+    avatar: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=400&h=400&fit=crop",
+    hasStory: true,
+  },
+  {
+    id: "4",
+    username: "Bhuvi.S",
+    avatar: "https://images.unsplash.com/photo-1517841905240-472988babdf9?w=400&h=400&fit=crop",
+    hasStory: true,
+  },
+  {
+    id: "5",
+    username: "Kavin.K",
+    avatar: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=400&h=400&fit=crop",
+    hasStory: true,
+  },
+  {
+    id: "6",
+    username: "Dinesh.A",
+    avatar: "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?w=400&h=400&fit=crop",
+    hasStory: true,
+  },
+  {
+    id: "7",
+    username: "Thrisha.S",
+    avatar: "https://images.unsplash.com/photo-1701615004837-40d8573b6652?w=400&h=400&fit=crop",
+    hasStory: true,
+  },
+  {
+    id: "8",
+    username: "Kiruba.rs",
+    avatar: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=400&h=400&fit=crop",
+    hasStory: true,
+  },
+];
